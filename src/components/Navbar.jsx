e7a1f3e28009df8730a74c092d8f0d7650085c0d
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2022-12-06 18:03:10 +0000

    ready to use

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from "react"
 import { Button, Container, Navbar, Modal } from "react-bootstrap"
 import { CartContext } from "../CartContext"
+import CartProduct from "./CartProduct"
 
 const NavbarComponent = () => {
   const cart = useContext(CartContext)
@@ -23,7 +24,21 @@ const NavbarComponent = () => {
                 <Modal.Title>Shopping Cart</Modal.Title>
               </Modal.Header>
               <Modal.Body>
-                <h1>This is the modal body</h1>
+                {
+                  productsCount>0?
+                  <>
+                    <p>Items in your cart:</p>
+                    {cart.items.map((currentProduct,inx)=>(
+                      <CartProduct key={inx} id={currentProduct.id} quantity={currentProduct.quantity}></CartProduct>
+                    ))}
+                    <h1>Total:{cart.getTotalCost().toFixed(2)}</h1>
+                    <Button variant='success'>
+                      Purchase items!
+                    </Button>
+                  </>
+                  :
+                  <h1>There are no items in ypour cart</h1>
+                }
               </Modal.Body>
             </Modal>
         </>
